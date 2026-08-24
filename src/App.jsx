33b573cc396ec7bc5f499701f850d35b6bddb271
Mohit Chauhan <mohitchauhanwrk@gmail.com>
2026-08-24 18:42:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-500">
+          <h1>Task Manager App</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
